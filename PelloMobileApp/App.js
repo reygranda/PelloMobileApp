e289e671fea48eb7724f5d970d92edab14e96cf6
Rey <Rey@Stars-MBP.attlocal.net>
2022-03-16 00:31:52 -0400

    Started Creating Landing View

--- PelloMobileApp/App.js
+++ PelloMobileApp/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Landing from "./components/Landing";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Landing"
+          component={Landing}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
---
+++ PelloMobileApp/components/Landing.js
@@ -0,0 +1,87 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  Assets,
+  Colors,
+  Spacings,
+  Typography,
+  Text,
+  Button,
+  Keyboard,
+  Incubator,
+} from "react-native-ui-lib"; //eslint-disable-line
+import { SafeAreaView } from "react-native-safe-area-context";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+const { TextField } = Incubator;
+
+export default function Landing() {
+  const [fullname, onChangeFullName] = React.useState(null);
+  const [email, onChangeEmail] = React.useState(null);
+  const [password, onChangePassword] = React.useState(null);
+
+  return (
+    <KeyboardAwareScrollView
+      extraHeight={25}
+      contentContainerStyle={styles.container}
+    >
+      <Text style={styles.text}>Pello</Text>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeFullName}
+          value={fullname}
+          placeholder="Enter Full Name"
+          keyboardType="default"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="Enter Email"
+          keyboardType="default"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="Enter Password"
+          keyboardType="default"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  input: {
+    height: 40,
+    width: 250,
+    marginBottom: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingRight: 50,
+    paddingLeft: 20,
+    borderColor: "#e7e7f7",
+  },
+  text: {
+    fontSize: 44,
+    fontWeight: "bold",
+    marginTop: 100,
+    marginBottom: 150,
+  },
+});
